6b37e42f36fde8a6cd11ef0acc024a40eb29280c
YourGuruNet <arvisiljins@gmail.com>
2023-10-06 16:47:56 +0300

    CRUD with Entity framework

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -2,33 +2,42 @@ namespace net7.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
-        private static List<Character> characters = new() {
-            new Character {Id = 0},
-            new Character {Id = 1, Name = "Sam"},
-            new Character {Id = 2, Name = "Arv"},
-        };
-        private readonly IMapper mapper;
+        private readonly IMapper _mapper;
+        private readonly DataContext _dataContext;
 
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper ,DataContext dataContext)
         {
-            this.mapper = mapper;
+            _dataContext = dataContext;
+            _mapper = mapper;
         }
 
-        public async  Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto character)
+        public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto character)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            var newCharacter = mapper.Map<Character>(character);
-            newCharacter.Id = characters.Count + 1;
-             characters.Add(newCharacter);
-             serviceResponse.Data = characters.Select(c => mapper.Map<GetCharacterDto>(c)).ToList();
-             return serviceResponse;
+
+            try
+            {
+                var newCharacter = _mapper.Map<Character>(character);
+                _dataContext.Characters.Add(newCharacter);
+                await _dataContext.SaveChangesAsync(); 
+                var dbCharacters = await _dataContext.Characters.ToListAsync();
+                serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = $"Error adding character to the database. Error - {ex}";
+            }
+
+            return serviceResponse;
         }
 
         public async  Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
+            var dbCharacters = await _dataContext.Characters.ToListAsync();
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>
             {
-                Data = characters.Select(c => mapper.Map<GetCharacterDto>(c)).ToList()
+                Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
             };
             return serviceResponse;
         }
@@ -36,18 +45,20 @@ namespace net7.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(character => character.Id == id);
-            serviceResponse.Data = mapper.Map<GetCharacterDto>(character);
+            var character = await _dataContext.Characters.FirstOrDefaultAsync(character => character.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             return serviceResponse;  
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto character) 
         {   
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            try {
-                var newCharacter = characters.FirstOrDefault(c => c.Id == character.Id) ?? throw new Exception($"Characters Id is incorrect");
-                mapper.Map(character, newCharacter);
-                serviceResponse.Data = mapper.Map<GetCharacterDto>(newCharacter);
+            try 
+            {
+                var newCharacter = await _dataContext.Characters.FirstOrDefaultAsync(c => c.Id == character.Id) ?? throw new Exception($"Characters Id is incorrect");
+                _mapper.Map(character, newCharacter);
+                await _dataContext.SaveChangesAsync(); 
+                serviceResponse.Data = _mapper.Map<GetCharacterDto>(newCharacter);
                 return serviceResponse; 
             } 
             catch (Exception ex)
@@ -63,10 +74,10 @@ namespace net7.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
             try 
             {
-                var character = characters.First(character => character.Id == id) ?? throw new Exception($"Characters Id is incorrect");
-                characters.Remove(character);
-
-                serviceResponse.Data = characters.Select(c => mapper.Map<GetCharacterDto>(c)).ToList();
+                var character = await _dataContext.Characters.FirstAsync(character => character.Id == id) ?? throw new Exception($"Characters Id is incorrect");
+                _dataContext.Characters.Remove(character);
+                await _dataContext.SaveChangesAsync();
+                serviceResponse.Data = await _dataContext.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
                 return serviceResponse; 
             } 
             catch (Exception ex)
